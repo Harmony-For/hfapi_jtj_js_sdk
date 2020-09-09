37a832272ddcfd6c2ad270853d9a20fd5e918859
nakata yuki <nasutaro211@gmail.com>
2020-09-09 16:26:31 +0900

    手動でいったん書いてみた

---
+++ index.js
@@ -0,0 +1,3 @@
+import GetMasterDataQueryService from "./lib/query_services/get_master_data.js";
+import UserSignUpCommandService from "./lib/command_services/user__sign_up.js";
+export { GetMasterDataQueryService, UserSignUpCommandService };
