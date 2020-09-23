d5f7e5394f3358a6b58cc85f0c0a593d221cff2b
nakata yuki <nasutaro211@gmail.com>
2020-09-23 11:31:24 +0900

    update

--- test/test.js
+++ test/test.js
@@ -1,11 +1,32 @@
+import { hfapiAxios } from "../index.js";
 import { GetMasterDataQueriesService } from "../index.js";
+let host = "http://localhost:3000/";
+
+hfapiAxios.interceptors.request.use((config) => {
+  config.paramsSerializer = (params) => {
+    // Qs is already included in the Axios package
+    return Qs.stringify(params, {
+      arrayFormat: "brackets",
+      encode: false,
+    });
+  };
+  config.baseURL = host;
+  return config;
+});
+
+
+console.log(hfapiAxios == GetMasterDataQueriesService.axios);
+console.log(hfapiAxios == GetMasterDataQueriesService.axios);
+console.log(hfapiAxios == GetMasterDataQueriesService.axios);
 
 const params = new GetMasterDataQueriesService.Params();
 
-console.log(params)
-
 params.target.countries = true
 
+console.log(params)
+
 GetMasterDataQueriesService.execute(params).then(resp=>{
-  console.log(resp)
+  console.log(resp.data)
+}).catch(resp=>{
+  console.log(resp.config);
 })
