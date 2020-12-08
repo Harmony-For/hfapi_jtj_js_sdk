52250c2969d811aa21a079240d283e33f3289ebb
H0R15H0 <shohori13@gmail.com>
2020-12-08 15:18:58 +0900

    update

--- index.js
+++ index.js
@@ -1,130 +1,132 @@
 
 import UserSaveResumeForApplicationCommandService from "./lib/commands_services/user__save_resume_for_application.js";
 
-import UserSignUpCommandService from "./lib/commands_services/user__sign_up.js";
+import UserSubmitResumeToJTJCommandService from "./lib/commands_services/user__submit_resume_to_jtj.js";
+
+import UserChangePageLanguageCommandService from "./lib/commands_services/user__change_page_language.js";
 
 import UserUploadResumeFaceImageCommandService from "./lib/commands_services/user__upload_resume_face_image.js";
 
-import UserSubmitResumeToJTJCommandService from "./lib/commands_services/user__submit_resume_to_jtj.js";
+import UserSignUpCommandService from "./lib/commands_services/user__sign_up.js";
 
 import UserApplyCommandService from "./lib/commands_services/user__apply.js";
 
 import UserUpdateProfileCommandService from "./lib/commands_services/user__update_profile.js";
 
+import UserSendInquiryCommandService from "./lib/commands_services/user__send_inquiry.js";
+
 import UserInitializeProfileCommandService from "./lib/commands_services/user__initialize_profile.js";
-
-import UserChangePageLanguageCommandService from "./lib/commands_services/user__change_page_language.js";
-
-import HfMemberSendRecommendMailCommandService from "./lib/commands_services/hf_member__send_recommend_mail.js";
-
-import HfMemberRegisterCompanyCommandService from "./lib/commands_services/hf_member__register_company.js";
-
-import HfMemberSaveUserAppraisalCommandService from "./lib/commands_services/hf_member__save_user_appraisal.js";
-
-import HfMemberSaveUserActionMemoCommandService from "./lib/commands_services/hf_member__save_user_action_memo.js";
 
 import HfMemberRegisterResumeResultCommandService from "./lib/commands_services/hf_member__register_resume_result.js";
 
-import HfMemberCreateResumeGoogleDocsCommandService from "./lib/commands_services/hf_member__create_resume_google_docs.js";
+import HfMemberSaveUserAppraisalCommandService from "./lib/commands_services/hf_member__save_user_appraisal.js";
 
-import HfMemberDeclineApplicationCommandService from "./lib/commands_services/hf_member__decline_application.js";
+import HfMemberSendResumeToCompanyCommandService from "./lib/commands_services/hf_member__send_resume_to_company.js";
+
+import HfMemberSetAcceptOfferCommandService from "./lib/commands_services/hf_member__set_accept_offer.js";
+
+import HfMemberStopRecommendRemindCommandService from "./lib/commands_services/hf_member__stop_recommend_remind.js";
+
+import HfMemberSaveUserActionMemoCommandService from "./lib/commands_services/hf_member__save_user_action_memo.js";
+
+import HfMemberSendRecommendMailCommandService from "./lib/commands_services/hf_member__send_recommend_mail.js";
 
 import HfMemberAddHfRemarkToResumeCommandService from "./lib/commands_services/hf_member__add_hf_remark_to_resume.js";
 
-import HfMemberRegisterOwnSmtpSettingCommandService from "./lib/commands_services/hf_member__register_own_smtp_setting.js";
-
-import HfMemberRegisterApplicationPassedCommandService from "./lib/commands_services/hf_member__register_application_passed.js";
-
-import HfMemberRegisterRecruitmentCommandService from "./lib/commands_services/hf_member__register_recruitment.js";
-
-import HfMemberSendRecommendRemindMailCommandService from "./lib/commands_services/hf_member__send_recommend_remind_mail.js";
-
-import HfMemberAdvanceApplicationToNextInterviewCommandService from "./lib/commands_services/hf_member__advance_application_to_next_interview.js";
-
-import HfMemberRegisterArticleCommandService from "./lib/commands_services/hf_member__register_article.js";
-
-import HfMemberRegisterInterviewResultCommandService from "./lib/commands_services/hf_member__register_interview_result.js";
-
 import HfMemberRegisterEventCommandService from "./lib/commands_services/hf_member__register_event.js";
 
-import HfMemberAssignHfMemberToUserCommandService from "./lib/commands_services/hf_member__assign_hf_member_to_user.js";
-
-import HfMemberCorrectResumeCommandService from "./lib/commands_services/hf_member__correct_resume.js";
-
-import HfMemberAssignHfMemberToApplicationCommandService from "./lib/commands_services/hf_member__assign_hf_member_to_application.js";
+import HfMemberRegisterRecruitmentCommandService from "./lib/commands_services/hf_member__register_recruitment.js";
 
 import HfMemberRegisterInterviewDetailsCommandService from "./lib/commands_services/hf_member__register_interview_details.js";
 
 import HfMemberSetRecommendMailCommandService from "./lib/commands_services/hf_member__set_recommend_mail.js";
 
-import HfMemberSetAcceptOfferCommandService from "./lib/commands_services/hf_member__set_accept_offer.js";
+import HfMemberRegisterArticleCommandService from "./lib/commands_services/hf_member__register_article.js";
 
-import HfMemberSendResumeToCompanyCommandService from "./lib/commands_services/hf_member__send_resume_to_company.js";
+import HfMemberRegisterCompanyCommandService from "./lib/commands_services/hf_member__register_company.js";
+
+import HfMemberRegisterInterviewResultCommandService from "./lib/commands_services/hf_member__register_interview_result.js";
+
+import HfMemberAssignHfMemberToUserCommandService from "./lib/commands_services/hf_member__assign_hf_member_to_user.js";
+
+import HfMemberRegisterApplicationPassedCommandService from "./lib/commands_services/hf_member__register_application_passed.js";
+
+import HfMemberAdvanceApplicationToNextInterviewCommandService from "./lib/commands_services/hf_member__advance_application_to_next_interview.js";
+
+import HfMemberDeclineApplicationCommandService from "./lib/commands_services/hf_member__decline_application.js";
+
+import HfMemberAssignHfMemberToApplicationCommandService from "./lib/commands_services/hf_member__assign_hf_member_to_application.js";
+
+import HfMemberCorrectResumeCommandService from "./lib/commands_services/hf_member__correct_resume.js";
+
+import HfMemberRegisterOwnSmtpSettingCommandService from "./lib/commands_services/hf_member__register_own_smtp_setting.js";
+
+import HfMemberCreateResumeGoogleDocsCommandService from "./lib/commands_services/hf_member__create_resume_google_docs.js";
 
 import HfMemberSendMailToUserCommandService from "./lib/commands_services/hf_member__send_mail_to_user.js";
 
-import HfMemberStopRecommendRemindCommandService from "./lib/commands_services/hf_member__stop_recommend_remind.js";
+import HfMemberSendRecommendRemindMailCommandService from "./lib/commands_services/hf_member__send_recommend_remind_mail.js";
 
-import UserShowOwnApplicationsQueryService from "./lib/queries_services/user__show_own_applications.js";
-
-import UserGetOwnDataQueryService from "./lib/queries_services/user__get_own_data.js";
-
-import UserGetApplicationResumePdfQueryService from "./lib/queries_services/user__get_application_resume_pdf.js";
-
-import GetNewRecruitmentsQueryService from "./lib/queries_services/get_new_recruitments.js";
-
-import GetArticlesQueryService from "./lib/queries_services/get_articles.js";
+import GetEventsQueryService from "./lib/queries_services/get_events.js";
 
 import GetUniversitiesQueryService from "./lib/queries_services/get_universities.js";
 
+import GetNewRecruitmentsQueryService from "./lib/queries_services/get_new_recruitments.js";
+
 import SearchRecruitmentsQueryService from "./lib/queries_services/search_recruitments.js";
 
-import AggregatesHfMemberQueryService from "./lib/queries_services/aggregates__hf_member.js";
+import GetRecruitmentRequiredLanguagesQueryService from "./lib/queries_services/get_recruitment_required_languages.js";
 
-import AggregatesArticleQueryService from "./lib/queries_services/aggregates__article.js";
+import UserGetOwnDataQueryService from "./lib/queries_services/user__get_own_data.js";
+
+import UserShowOwnApplicationsQueryService from "./lib/queries_services/user__show_own_applications.js";
+
+import UserGetApplicationResumePdfQueryService from "./lib/queries_services/user__get_application_resume_pdf.js";
+
+import GetArticlesQueryService from "./lib/queries_services/get_articles.js";
+
+import GetMasterDataQueryService from "./lib/queries_services/get_master_data.js";
+
+import HfMemberGetCompanyOptionsQueryService from "./lib/queries_services/hf_member__get_company_options.js";
+
+import HfMemberSearchApplicationsQueryService from "./lib/queries_services/hf_member__search_applications.js";
+
+import HfMemberSearchRecruitmentsQueryService from "./lib/queries_services/hf_member__search_recruitments.js";
+
+import HfMemberGetOwnDataQueryService from "./lib/queries_services/hf_member__get_own_data.js";
+
+import HfMemberGetFullOwnDataQueryService from "./lib/queries_services/hf_member__get_full_own_data.js";
+
+import HfMemberShowApplicationDetailQueryService from "./lib/queries_services/hf_member__show_application_detail.js";
+
+import HfMemberShowUserQueryService from "./lib/queries_services/hf_member__show_user.js";
+
+import HfMemberGetRecommendMailsQueryService from "./lib/queries_services/hf_member__get_recommend_mails.js";
+
+import HfMemberGetCompanyOfficeOptionsQueryService from "./lib/queries_services/hf_member__get_company_office_options.js";
+
+import HfMemberSearchUsersQueryService from "./lib/queries_services/hf_member__search_users.js";
+
+import HfMemberGetRecommendMailQueryService from "./lib/queries_services/hf_member__get_recommend_mail.js";
+
+import AggregatesCompanyQueryService from "./lib/queries_services/aggregates__company.js";
 
 import AggregatesApplicationQueryService from "./lib/queries_services/aggregates__application.js";
 
-import AggregatesRecommendMailQueryService from "./lib/queries_services/aggregates__recommend_mail.js";
+import AggregatesArticleQueryService from "./lib/queries_services/aggregates__article.js";
 
-import AggregatesUserQueryService from "./lib/queries_services/aggregates__user.js";
+import AggregatesHfMemberQueryService from "./lib/queries_services/aggregates__hf_member.js";
 
 import AggregatesRecruitmentQueryService from "./lib/queries_services/aggregates__recruitment.js";
+
+import AggregatesRecommendMailQueryService from "./lib/queries_services/aggregates__recommend_mail.js";
 
 import AggregatesEventQueryService from "./lib/queries_services/aggregates__event.js";
 
 import AggregatesMailTemplateQueryService from "./lib/queries_services/aggregates__mail_template.js";
 
-import AggregatesCompanyQueryService from "./lib/queries_services/aggregates__company.js";
-
-import GetMasterDataQueryService from "./lib/queries_services/get_master_data.js";
-
-import GetEventsQueryService from "./lib/queries_services/get_events.js";
-
-import GetRecruitmentRequiredLanguagesQueryService from "./lib/queries_services/get_recruitment_required_languages.js";
-
-import HfMemberGetCompanyOfficeOptionsQueryService from "./lib/queries_services/hf_member__get_company_office_options.js";
-
-import HfMemberGetOwnDataQueryService from "./lib/queries_services/hf_member__get_own_data.js";
-
-import HfMemberShowUserQueryService from "./lib/queries_services/hf_member__show_user.js";
-
-import HfMemberGetCompanyOptionsQueryService from "./lib/queries_services/hf_member__get_company_options.js";
-
-import HfMemberGetRecommendMailsQueryService from "./lib/queries_services/hf_member__get_recommend_mails.js";
-
-import HfMemberSearchUsersQueryService from "./lib/queries_services/hf_member__search_users.js";
-
-import HfMemberSearchRecruitmentsQueryService from "./lib/queries_services/hf_member__search_recruitments.js";
-
-import HfMemberSearchApplicationsQueryService from "./lib/queries_services/hf_member__search_applications.js";
-
-import HfMemberShowApplicationDetailQueryService from "./lib/queries_services/hf_member__show_application_detail.js";
-
-import HfMemberGetFullOwnDataQueryService from "./lib/queries_services/hf_member__get_full_own_data.js";
-
-import HfMemberGetRecommendMailQueryService from "./lib/queries_services/hf_member__get_recommend_mail.js";
+import AggregatesUserQueryService from "./lib/queries_services/aggregates__user.js";
 
 import hfapiAxios from "./lib/axios.js"
 import Aggregates from "./lib/aggregates/index.js"
-export { hfapiAxios, Aggregates, UserSaveResumeForApplicationCommandService, UserSignUpCommandService, UserUploadResumeFaceImageCommandService, UserSubmitResumeToJTJCommandService, UserApplyCommandService, UserUpdateProfileCommandService, UserInitializeProfileCommandService, UserChangePageLanguageCommandService, HfMemberSendRecommendMailCommandService, HfMemberRegisterCompanyCommandService, HfMemberSaveUserAppraisalCommandService, HfMemberSaveUserActionMemoCommandService, HfMemberRegisterResumeResultCommandService, HfMemberCreateResumeGoogleDocsCommandService, HfMemberDeclineApplicationCommandService, HfMemberAddHfRemarkToResumeCommandService, HfMemberRegisterOwnSmtpSettingCommandService, HfMemberRegisterApplicationPassedCommandService, HfMemberRegisterRecruitmentCommandService, HfMemberSendRecommendRemindMailCommandService, HfMemberAdvanceApplicationToNextInterviewCommandService, HfMemberRegisterArticleCommandService, HfMemberRegisterInterviewResultCommandService, HfMemberRegisterEventCommandService, HfMemberAssignHfMemberToUserCommandService, HfMemberCorrectResumeCommandService, HfMemberAssignHfMemberToApplicationCommandService, HfMemberRegisterInterviewDetailsCommandService, HfMemberSetRecommendMailCommandService, HfMemberSetAcceptOfferCommandService, HfMemberSendResumeToCompanyCommandService, HfMemberSendMailToUserCommandService, HfMemberStopRecommendRemindCommandService, UserShowOwnApplicationsQueryService, UserGetOwnDataQueryService, UserGetApplicationResumePdfQueryService, GetNewRecruitmentsQueryService, GetArticlesQueryService, GetUniversitiesQueryService, SearchRecruitmentsQueryService, AggregatesHfMemberQueryService, AggregatesArticleQueryService, AggregatesApplicationQueryService, AggregatesRecommendMailQueryService, AggregatesUserQueryService, AggregatesRecruitmentQueryService, AggregatesEventQueryService, AggregatesMailTemplateQueryService, AggregatesCompanyQueryService, GetMasterDataQueryService, GetEventsQueryService, GetRecruitmentRequiredLanguagesQueryService, HfMemberGetCompanyOfficeOptionsQueryService, HfMemberGetOwnDataQueryService, HfMemberShowUserQueryService, HfMemberGetCompanyOptionsQueryService, HfMemberGetRecommendMailsQueryService, HfMemberSearchUsersQueryService, HfMemberSearchRecruitmentsQueryService, HfMemberSearchApplicationsQueryService, HfMemberShowApplicationDetailQueryService, HfMemberGetFullOwnDataQueryService, HfMemberGetRecommendMailQueryService };
+export { hfapiAxios, Aggregates, UserSaveResumeForApplicationCommandService, UserSubmitResumeToJTJCommandService, UserChangePageLanguageCommandService, UserUploadResumeFaceImageCommandService, UserSignUpCommandService, UserApplyCommandService, UserUpdateProfileCommandService, UserSendInquiryCommandService, UserInitializeProfileCommandService, HfMemberRegisterResumeResultCommandService, HfMemberSaveUserAppraisalCommandService, HfMemberSendResumeToCompanyCommandService, HfMemberSetAcceptOfferCommandService, HfMemberStopRecommendRemindCommandService, HfMemberSaveUserActionMemoCommandService, HfMemberSendRecommendMailCommandService, HfMemberAddHfRemarkToResumeCommandService, HfMemberRegisterEventCommandService, HfMemberRegisterRecruitmentCommandService, HfMemberRegisterInterviewDetailsCommandService, HfMemberSetRecommendMailCommandService, HfMemberRegisterArticleCommandService, HfMemberRegisterCompanyCommandService, HfMemberRegisterInterviewResultCommandService, HfMemberAssignHfMemberToUserCommandService, HfMemberRegisterApplicationPassedCommandService, HfMemberAdvanceApplicationToNextInterviewCommandService, HfMemberDeclineApplicationCommandService, HfMemberAssignHfMemberToApplicationCommandService, HfMemberCorrectResumeCommandService, HfMemberRegisterOwnSmtpSettingCommandService, HfMemberCreateResumeGoogleDocsCommandService, HfMemberSendMailToUserCommandService, HfMemberSendRecommendRemindMailCommandService, GetEventsQueryService, GetUniversitiesQueryService, GetNewRecruitmentsQueryService, SearchRecruitmentsQueryService, GetRecruitmentRequiredLanguagesQueryService, UserGetOwnDataQueryService, UserShowOwnApplicationsQueryService, UserGetApplicationResumePdfQueryService, GetArticlesQueryService, GetMasterDataQueryService, HfMemberGetCompanyOptionsQueryService, HfMemberSearchApplicationsQueryService, HfMemberSearchRecruitmentsQueryService, HfMemberGetOwnDataQueryService, HfMemberGetFullOwnDataQueryService, HfMemberShowApplicationDetailQueryService, HfMemberShowUserQueryService, HfMemberGetRecommendMailsQueryService, HfMemberGetCompanyOfficeOptionsQueryService, HfMemberSearchUsersQueryService, HfMemberGetRecommendMailQueryService, AggregatesCompanyQueryService, AggregatesApplicationQueryService, AggregatesArticleQueryService, AggregatesHfMemberQueryService, AggregatesRecruitmentQueryService, AggregatesRecommendMailQueryService, AggregatesEventQueryService, AggregatesMailTemplateQueryService, AggregatesUserQueryService };
